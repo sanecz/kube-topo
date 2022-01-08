ce32219e1334005e10d7338188920aa906bf15a7
Lisa Bekdache <lisa.bekdache@shadow.tech>
2022-01-08 13:19:03 +0100

    Remove the regexp

--- topo.py
+++ topo.py
@@ -1,79 +1,89 @@
 import argparse
-import logging
-from functools import reduce, partial, partialmethod
-import re
-import subprocess
-import ipaddress
-import concurrent.futures
-import os
-import networkx as nx
 import hashlib
-from pyvis.network import Network
+import ipaddress
+import logging
+import networkx as nx
+
+from functools import reduce, partial, partialmethod
 from kubernetes import client, config
 from kubernetes.stream import stream
-import traceback
-logger = None
+from parse import parse
+from pyvis.network import Network
+
+logger = logging.getLogger(__name__)
+
+# https://www.kernel.org/doc/Documentation/networking/proc_net_tcp.txt
+NET_FORMAT = (
+    "{id:>4d}: {src:x}:{srcp:04x} {rem:x}:{remp:04x} {state:02x} "
+    "{tx_queue:08x}:{rx_queue:08x} {timer_active:02x}:{timer_expires:08x} "
+    "{retransmit:08x} {uid:>5d} {probes:>8d} {inode:d} {refcount:d} {pointer}"
+)
+
+TCP_FORMAT = NET_FORMAT + " {rto:d} {ato:d} {qopm:d} {cwindow:d} {slowstart:d}"
+UDP_FORMAT = NET_FORMAT + " {drops:d}"
 
 
-REGEXP = re.compile(r'(?P<ip_listen>(?:[a-fA-F0-9:]+:)?(?:(?:[0-9]\.?){1,3}){4})(?::)(?P<port_listen>[0-9]{1,6}|\*)(?:\s+)(?P<ip_foreign>(?:[a-fA-F0-9:]+:)?(?:(?:[0-9]\.?){1,3}){4})(?::)(?P<port_foreign>[0-9]{1,6}|\*)(?:\s+)')
+def _swap_endianess(integer):
+    return int.from_bytes(integer.to_bytes(4, byteorder='little'), byteorder='big', signed=False)
 
 def _parse_netstat(output):
     connections = {}
-    output = output.strip().split("\n")
 
-    for line in output:
-        if line.startswith("tcp") or line.startswith("udp"):
+    for line in output.split("\n"):
+        line = line.strip()
+        parsed_line = None
+        # fallback to NET_FORMAT when everything fails ?
+        for fmt_type in [TCP_FORMAT, UDP_FORMAT, NET_FORMAT]:
             try:
-                line = re.search(REGEXP, line).groupdict()
-            except:
-                logger.trace(f"Line '{line}' is not matching regexp")
+                parsed_line = parse(fmt_type, line).named
+                break
+            except AttributeError:
                 continue
+        else:
+            logger.warning(f"Line: {line} not parsed")
+            continue
 
+        ip_src = ipaddress.ip_address(_swap_endianess(parsed_line["src"]))
+        ip_rem = ipaddress.ip_address(_swap_endianess(parsed_line["rem"]))
 
-            ip_src = ipaddress.ip_address(line["ip_listen"])
-            ip_dst = ipaddress.ip_address(line["ip_foreign"])
-            logger.trace(f"Found: '{ip_src}' / '{ip_dst}'")
-    
-            try:
-                ip_src = ip_src.ipv4_mapped
-                logger.trace(f"IP src '{ip_src}' is an ipv6 or 6to4, converting")
-            except:
-                logger.trace(f"IP src '{ip_src}' is an ipv4")
-            try:
-                ip_dst = ip_dst.ipv4_mapped
-                logger.trace(f"IP dst '{ip_dst}' is an ipv6 or 6to4, converting")
-            except:
-                logger.trace(f"IP dst '{ip_dst}' is an ipv4")
+        # FIXME: state listen
+        if str(ip_rem) == '127.0.0.1' or str(ip_src) == '127.0.0.1' or str(ip_src) == '0.0.0.0' or str(ip_rem) == '0.0.0.0':
+            logger.info(f"Ignoring {ip_src} :: {ip_rem} , local address or listen")
+            continue
 
-            if str(ip_dst) == '127.0.0.1' or str(ip_src) == '127.0.0.1' or str(ip_src) == '0.0.0.0':
-                logger.trace(f"Ignoring {ip_src} :: {ip_dst} , local address or listen")
-                continue
+        # FIXME: allowlist
+        if not ip_rem.is_private:
+            logger.debug(f"Ignoring {ip_rem} because it's not a private ip")
+            continue
 
-
-            if not ip_dst.is_private:
-                logger.critical(f"Ignoring {ip_dst} because it's not a private ip")
-                continue
-
-            if int(line['port_listen']) > 32768:
-                pass #plustard
-            else:
-                print(f"{ip_dst}")
-                connections.setdefault(f"{line['port_listen']}", []).append(str(ip_dst))
+        # sysctl net.ipv4.ip_local_port_range
+        # FIXME: cfg
+        if 32768 < parsed_line['srcp'] < 60999:
+            # we are connected to something possibly
+            pass #plustard
+        else:
+            connections.setdefault(f"{parsed_line['srcp']}", []).append(str(ip_rem))
 
     connections = {k: list(set(v)) for k, v in connections.items()}
 
     return connections
 
 
-def _run_netstats(kube_api, namespace, pod):
+def _get_netstats(kube_api, namespace, pod):
     logger.info(f"Retrieving netstats for {namespace}/{pod.metadata.name}")
 
     connections = {}
     for container in pod.spec.containers:
         container = container.name
         logger.info(f"Testing: {namespace}/{pod.metadata.name}/{container}")
-        exec_netstat_command = ['netstat', '-npatu']
-        exec_ss_command = ['ss', '-npatu']
+        # FIXME: support ipv6 later
+        exec_netstat_command = [
+            'cat',
+            '/proc/net/tcp',
+#            '/proc/net/tcp6',
+            '/proc/net/udp',
+#            '/proc/net/udp6'
+        ]
         try:
             resp = stream(
                 kube_api.connect_get_namespaced_pod_exec,
@@ -82,18 +92,12 @@ def _run_netstats(kube_api, namespace, pod):
                 stderr=True, stdin=False,
                 stdout=True, tty=False
             )
-            if "file not found" in resp:
-                resp = stream(
-                    kube_api.connect_get_namespaced_pod_exec,
-                    name=pod.metadata.name, namespace=namespace, container=container,
-                    command=exec_ss_command,
-                    stderr=True, stdin=False,
-                    stdout=True, tty=False
-                )
-            logger.debug(_parse_netstat(resp))
-            connections |= _parse_netstat(resp)
         except Exception as e:
             logger.error(f"Cannot connect to {namespace}/{pod.metadata.name}/{container}: '{e}'")
+        else:
+            logger.debug(_parse_netstat(resp))
+            connections |= _parse_netstat(resp)
+
 
         logger.debug(connections)
 
@@ -104,7 +108,7 @@ def get_netstats(kube_api, pod_list):
     for group_name, pods in pod_list.copy().items():
         for i, pod in enumerate(pods["pods"]):
             pod_list[group_name].setdefault("connections", {})
-            pod_list[group_name]["connections"] |= _run_netstats(kube_api, pods["namespace"], pod)
+            pod_list[group_name]["connections"] |= _get_netstats(kube_api, pods["namespace"], pod)
 
     return pod_list
 
@@ -117,6 +121,7 @@ def get_pods(kube_api, namespaces, all_ns):
         if elem.metadata.namespace not in namespaces:
             continue
         short_pod_name = elem.metadata.name.split("-")
+        # FIXME: find a better way to get base name of a pod
         short_pod_name = "-".join(
             short_pod_name[:-2] if len(short_pod_name[-1]) == 5 and len(short_pod_name[-2]) >= 9 else short_pod_name
         )
@@ -133,7 +138,6 @@ def get_pods(kube_api, namespaces, all_ns):
         logger.debug(f"{short_pod_name} {elem.metadata.name}")
         pods[short_pod_name]["pods"].append(elem)
         pods[short_pod_name]["ips"].append(elem.status.pod_ip)
-
     
     return get_netstats(kube_api, pods)
 
@@ -145,12 +149,11 @@ def get_pod_by_ip(pods_list, ip, all_ns):
     for item in all_ns.items:
         if ip == item.status.pod_ip:
             short_pod_name = item.metadata.name.split("-")
+            # FIXME: find a better way to get base name of a pod
             short_pod_name = "-".join(
                 short_pod_name[:-2] if len(short_pod_name[-1]) == 5 and len(short_pod_name[-2]) >= 9 else short_pod_name
             )            
             return f'{item.metadata.namespace}/{short_pod_name}'
-         
-            
 
 def draw_pods(namespaces):
     config.load_kube_config()
@@ -160,17 +163,20 @@ def draw_pods(namespaces):
 
     net = Network('768px', '1366px', directed=True)
     # create all nodes firsts
+    # TODO: extend class to put more informations inside each nodes
     for group_name, pods in pods_list.items():
         color = hashlib.md5(bytes(pods["namespace"].encode("utf8"))).hexdigest()[:6]
         net.add_node(f"{pods['namespace']}/{group_name}", title=",".join(pods["ips"]), color=f"#{color}")
 
+    # FIXME: ignore stuff from kube-system or maybe ingress too
+    # configurable if possible
     for group_name, pods in pods_list.items():
         for port, ips in pods["connections"].items():
             for ip in ips:
                 dest_pod = get_pod_by_ip(pods_list, ip, all_ns)
                 try:
                     net.get_node(dest_pod)
-                except:
+                except: # FIXME: bad
                     if not dest_pod:
                         dest_pod = ip
                     if dest_pod.split("/")[0] == pods["namespace"]:
@@ -187,7 +193,7 @@ def draw_pods(namespaces):
                                  label=f":{port}", arrowStrikethrough=True)
 
     net.show("test.html")
-    
+
 
 def main():
     """
@@ -205,15 +211,6 @@ def main():
         args.namespaces
     )
 
-    logger = logging.getLogger()
-
-
-    logging.TRACE = 5
-    logging.addLevelName(logging.TRACE, 'TRACE')
-    logging.Logger.trace = partialmethod(logging.Logger.log, logging.TRACE)
-    logging.trace = partial(logging.log, logging.TRACE)
-    
-    logging.addLevelName(logging.TRACE, "\033[1;35m%s\033[1;0m" % logging.getLevelName(logging.TRACE))
     logging.addLevelName(logging.INFO, "\033[1;32m%s\033[1;0m" % logging.getLevelName(logging.INFO))
     logging.addLevelName(logging.DEBUG, "\033[1;34m%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
     logging.addLevelName(logging.WARNING, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
@@ -223,7 +220,6 @@ def main():
         logging.basicConfig(level=logging.getLevelName(args.verbose))
     else:
         logging.basicConfig(level=logging.WARNING)
-
 
     draw_pods(namespaces)
 
